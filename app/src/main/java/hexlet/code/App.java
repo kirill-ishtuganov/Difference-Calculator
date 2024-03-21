5b3243da86e7317a369e5f0689ad29d695a8f9e5
Kirill Ishtuganov <ian574miller@gmail.com>
2024-03-21 15:15:40 +0300

    add help and version options

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,18 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "Diff", version = "1.0", mixinStandardHelpOptions = true)
+public class App implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Hello, world!");
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
