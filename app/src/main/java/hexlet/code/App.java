c3aadb4b49716a210830e5ed6837b57345b3fd7d
Kirill Ishtuganov <ian574miller@gmail.com>
2024-03-21 15:44:40 +0300

    add new parameters and option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,16 +2,28 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "Diff", version = "1.0", mixinStandardHelpOptions = true)
+@Command(name = "Diff", version = "1.0", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
 
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+        private String filepath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+        private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+        int format;
     @Override
     public void run() {
         System.out.println("Hello, world!");
     }
 
     public static void main(String[] args) {
+
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
